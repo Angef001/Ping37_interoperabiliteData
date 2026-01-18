9feed5e2bc3b6a3ef113bcad8ba1fe2b92a3d81c
Mame Fatou <fallmamefatou09@gmail.com>
2026-01-18 14:31:52 +0100

    edsan_to_fhir

--- app/core/converters/export_edsan_to_fhir.py
+++ app/core/converters/export_edsan_to_fhir.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
 from edsan_to_fhir import export_eds_to_fhir
 
+load_dotenv()  # charge le .env
+
 export_eds_to_fhir(
-    eds_dir="C:/Users/DELL/OneDrive/Documents/projet_ping/Ping37_interoperabiliteData/eds",
-    output_dir="C:/Users/DELL/OneDrive/Documents/projet_ping/Ping37_interoperabiliteData/test_output_fhir",
-    bundle_strategy="patient"  # or "encounter"
+    eds_dir=os.getenv("EDS_DIR"),
+    output_dir=os.getenv("FHIR_OUTPUT_DIR"),
+    bundle_strategy=os.getenv("FHIR_BUNDLE_STRATEGY", "patient")
 )
+
+if not os.getenv("EDS_DIR") or not os.getenv("FHIR_OUTPUT_DIR"):
+    raise RuntimeError("Variables d'environnement manquantes (EDS_DIR / FHIR_OUTPUT_DIR)")
+
